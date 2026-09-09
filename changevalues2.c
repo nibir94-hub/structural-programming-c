73192502d12846817df7a6f6dc9dbef286dc3b91
abdullah <abdullahnibir32@gmail.com>
2026-09-09 14:36:00 +0600

    assaigning values from one variable to another

---
+++ changevalues2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main ()
+{
+int mynum= 10;
+int myothernum;
+
+myothernum = mynum ;//assaigning mynum to myothernum
+
+printf("my number is %d\n",mynum);
+
+printf("my other number is %d ",myothernum);
+
+return 0;
+}
