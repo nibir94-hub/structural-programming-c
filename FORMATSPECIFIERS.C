c70177082c8dda6b558623ea9cce7bd567b1c28c
abdullah <abdullahnibir32@gmail.com>
2026-09-09 14:13:27 +0600

    format specifier

---
+++ FORMATSPECIFIERS.C
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+int num = 21;
+float pi = 3.1416;
+double e = 2.718281828459045;
+char letter = 'A';
+char name[] = "abdullah";
+
+printf("integer = %d\n",num);
+
+printf("float = %f\n",pi);
+
+printf("double = %lf\n",e);
+
+printf("charecter = %c\n",letter);
+
+printf("string = %s\n",name);
+
+    return 0;
+}
