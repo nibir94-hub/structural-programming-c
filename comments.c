e4c3bbc3f00c402470ffa79239110963597fa338
abdullah <abdullahnibir32@gmail.com>
2026-09-09 11:56:44 +0600

    comments of diff types

---
+++ comments.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+//this is a comment
+printf("hellow world");\
+
+printf("my name is abdullah");//this is also a comment
+
+//printf("iam learning c programming"); comments can be used for dissabling codes
+
+/*this is a multiline comment and it is uSED FOR WRITING LONG COMMENTS*/
+printf("today we are writing comments in c programming");
+
+    return 0;
+}
