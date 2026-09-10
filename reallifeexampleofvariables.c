f8815af2e2c3708f72d1c07ebcef5a754bc31210
abdullah <abdullahnibir32@gmail.com>
2026-09-10 13:16:18 +0600

    real lifr example on varables

---
+++ reallifeexampleofvariables.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+
+// Student data
+int studentID = 32;
+int studentAge = 20;
+float studentFee = 75.25;
+char studentGrade = 'A';
+
+// Print variables
+printf("Student ID: %d\n", studentID);
+printf("Student age: %d\n", studentAge);
+printf("Student fee: %f\n", studentFee);
+printf("Student grade: %c", studentGrade);
+
+
+
+    return 0;
+}
